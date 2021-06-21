638508a53da6e0bde0eda447bc8adeb3755085ee
losidk <991807272@qq.com>
2021-06-21 09:27:03 +0800

    bind 方法体内不使用 apply、call 等辅助方法

--- javascript/bind.js
+++ javascript/bind.js
@@ -1,27 +1,32 @@
-// 自定义 bind 函数
+// 自定义 bind 方法
 function customBind() {
-    const arg = arguments
+    const args = arguments
     const self = this
     return function () {
-        const [_this, ...remainder] = arg
-        return self.apply(_this, [...remainder])
+        // console.log(this)
+        // 当前的 _this 即为上层函数体内调用方法的方法体
+        const [_this, ...reminder] = args
+
+        _this.__proto__.fn = self
+        _this.fn(...reminder, ...arguments)
+        delete _this.__proto__.fn
     }
 }
 
+// 这里有个疑问：直接赋值 Function.prototype.bind = customBind 和 在 Function 的 prototype 上添加新的 customBind 方法，所执行的结果并不相同
+// Function.prototype.customBind = customBind
 Function.prototype.bind = customBind
 
-function bindFunction() {
-    console.log(this, arguments)
-}
-
-const obj = {
+let obj = {
     a: 1
 }
 
-console.log(bindFunction.bind(obj, 23)());
-// console.log(bindFunction());
-// console.log(bindFunction.bind(null, 23)());
+function testFn(x, y) {
+    console.log(this)
+    console.log(x, y)
+}
 
-
-
+// const fn = testFn.customBind(obj, 1, 2, 3)
+const fn = testFn.bind(obj, 1, 2, 3)
+fn(4)
 
