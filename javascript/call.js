a767cd9380039c8d49942c132247f2f6e6782e2b
losidk <991807272@qq.com>
2021-06-21 09:42:25 +0800

    自定义 call 方法

---
+++ javascript/call.js
@@ -0,0 +1,26 @@
+// 自定义 call函数
+function customCall() {
+    const self = this;
+    const args = arguments;
+
+    return (function () {
+        const [_this, ...reminder] = args
+        _this.__proto__.fn = self;
+        _this.fn(...reminder)
+        delete _this.__proto__.fn;
+    })()
+}
+
+Function.prototype.call = customCall
+
+const obj = {
+    a: 1
+}
+
+function testFn() {
+    console.log(this)
+    console.log(arguments)
+}
+
+const res = testFn.call(obj, 1, 2, 4, 5)
+// console.log(res);
