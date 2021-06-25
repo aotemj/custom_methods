03fee4d50713cbe09a46191b1681897d86fcc0fd
losidk <991807272@qq.com>
2021-06-25 15:00:36 +0800

    冒泡排序

---
+++ javascript/sort/bubbleSort.js
@@ -0,0 +1,25 @@
+/**
+ * 冒泡排序
+ * @param arr
+ * @param direction
+ */
+function bubbleSort (arr, direction = 'asc') {
+  const l = arr.length
+  const symbol = direction === 'asc' ? '>' : '<'
+  let flag = false
+  for (let i = 0; i < l - 1; i++) {
+    flag = false
+    for (let j = 0; j < l - i - 1; j++) {
+      // eslint-disable-next-line no-eval
+      if (eval(`${arr[j]} ${symbol} ${arr[j + 1]}`)) {
+        const temp = arr[j]
+        arr[j] = arr[j + 1]
+        arr[j + 1] = temp
+        flag = true
+      }
+    }
+    if (!flag) break
+  }
+}
+
+module.exports.bubbleSort = bubbleSort
