d69b7b50503e0772930299786f5b173c034f12cb
losidk <991807272@qq.com>
2021-06-28 16:48:17 +0800

    自定义 shift 方法 优化（不使用Array 方法）

--- javascript/Array/shift.js
+++ javascript/Array/shift.js
@@ -1,6 +1,13 @@
+// 自定义 Array shift 方法
+// 方法会删除首个数组元素，并把所有其他元素“位移”到更低的索引
 const customShift = function () {
-  const [res] = this.splice(0, 1)
-  return res
+  const first = this[0]; const l = this.length
+  for (let i = 0; i < l - 1; i++) {
+    this[i] = this[i + 1]
+  }
+  this[l - 1] = null
+  this.length = l - 1
+  return first
 }
 
 Array.prototype.shift = customShift
